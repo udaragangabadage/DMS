dd870592d7580ca25f4f7bfa864a37ade5dbbc40
ishantha <ishanthaudara96@gmail.com>
2018-01-02 19:01:28 +0530

    ggggg

--- footer.php
+++ footer.php
@@ -25,7 +25,7 @@
 
 <body>
 
-hello world
+
 
 
   <!-- Optional JavaScript -->
--- header.php
+++ header.php
@@ -25,6 +25,78 @@
 
 <body>
 
+  <!-- nav start -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+
+      <a class="navbar-brand text-dark" href="#">
+      <!-- <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""> -->
+      <i class="fa fa-shield " aria-hidden="true"></i>
+      Department Of Disaster Management
+    </a>
+      <!-- <a class="navbar-brand" href="#">Department Of Disaster Management</a> -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <!-- <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li> -->
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Contact Us</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link text-white" href="#">Incident Report Center</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Infomation Center
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+
+
+          <!-- <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li> -->
+        </ul>
+        <!-- <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn btn-dark my-2 my-sm-0" type="submit">Search</button>
+          </form> -->
+          <ul class="nav justify-content-end">
+
+            <li class="nav-item">
+              <!-- <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn btn-dark my-2 my-sm-0" type="submit">Search</button>
+                </form> -->
+            </li>
+            <li class="nav-item">
+            <a href=""><button type="button" class="btn btn-dark my-2 my-sm-0">Logout</button></a>
+            </li>
+
+          </ul>
+      </div>
+      </div>
+    </nav>
+
+  <!-- nav end -->
 
 
 
