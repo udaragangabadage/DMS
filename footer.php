e8468d9a025c8b68543af56c90b4d71df029c1f8
ishantha <ishanthaudara96@gmail.com>
2018-01-03 06:38:20 +0530

    dfadlkjkda

--- footer.php
+++ footer.php
@@ -25,7 +25,64 @@
 
 <body>
 
+<footer class="navbar-dark bg-danger">
+  <br>
+  <div class="container">
+    <div class="row">
 
+      <div class="col-sm-2">
+        <h6 class="text-white">Copyright &copy; 2017</h6>
+      </div>
+
+      <div class="col-sm-2">
+        <h6 class="text-white">About Us</h6>
+        <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim aborum.</p>
+      </div>
+      <div class="col-sm-2">
+        <h6 class="text-white">Contact Us</h6>
+        <p class="text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+      </div>
+      <div class="col-sm-2">
+        <h6 class="text-white">Navigation</h6>
+        <ul class="unstyled">
+          <li class="text-white"> <a class="text-white"  href="#">Home</a> </li>
+          <li class="text-white"> <a class="text-white"  href="#">About Us</a> </li>
+          <li class="text-white"> <a class="text-white"  href="#">Contact Us</a> </li>
+          <li class="text-white"> <a class="text-white"  href="#">Incident Report Center</a> </li>
+          <li class="text-white"> <a class="text-white"  href="#">Information Center</a> </li>
+
+        </ul>
+      </div>
+      <div class="col-sm-2">
+          <h6 class="text-white">Follow us</h6>
+        <ul class="unstyled">
+          <li class="text-white"> <a class="text-white"  href="#">Twitter</a> </li>
+          <li class="text-white"> <a class="text-white"  href="#">Facebook</a> </li>
+          <li class="text-white"> <a class="text-white"  href="#">Instagram</a> </li>
+          <li class="text-white"> <a class="text-white"  href="#">Snapchat</a> </li>
+
+        </ul>
+      </div>
+      <div class="col-sm-2">
+        <h6 class="text-white">United We Stand.</h6>
+        <h6 class="text-white"> One Flag.One nation.</h6>
+
+
+
+      </div>
+
+    </div>
+
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h6>Coded with <span><i class="fa fa-heart" aria-hidden="true"></i></span> by Group 57</h6>
+     </div>
+   </div>
+  </div>
+  <br>
+</footer>
 
 
   <!-- Optional JavaScript -->
