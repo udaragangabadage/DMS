928ff069ed28ddad9e0ee5ea811d8e50d4b4fd0d
ishantha <ishanthaudara96@gmail.com>
2018-01-03 21:33:26 +0530

    adadadad

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,54 +1,51 @@
 <?php
+if(isset($_POST['submit'])){
 
-if (isset($_POST['submit'])){
-    include_once 'dbh.inc.php';
+  include_once 'dbh.inc.php';
 
+  $first = $_POST['first'];
+  $last = $_POST['last'];
+  $email = $_POST['email'];
+  $uid = $_POST['uid'];
+  $pwd = $_POST['pwd'];
 
-    $first = mysqli_real_escape_string($conn, $_POST['first']);
-    $last = mysqli_real_escape_string($conn, $_POST['last']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $uid = mysqli_real_escape_string($conn, $_POST['uid']);
-    $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+// Error handlers
+//chech for empty fields
 
-    //Error handlers
-    //Check for empty fields
-    if(empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)){
-      header("Location:../signup.php?signup=empty");
+if(empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd) ){
+  header("Location: ../signup.php?signup=empty");
+  exit();
+}else{
+  //check if input characters are valid
+  if (!preg_match("/^[a-zA-Z]*$/",$first) ||  !preg_match("/^[a-zA-Z]*$/",$last)){
+      header("Location: ../signup.php?signup=invalid");
       exit();
-    }else {
-      //Check if input characters are valid
-      if(!preg_match("/^[a-zA-Z]*$/",$first) ||  !preg_match("/^[a-zA-Z]*$/",$last)){
-        header("Location: ../signup.php?signup=invalid");
-        exit();
-      }else {
-        // //Chech if email is valid
-        // if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        //   header("Location: ../signup.php?signup=email");
-        //   exit();
-        }else {
-          $SQL = "SELECT * FROM users WHERE user_id='$uid'";
-          $result = mysqli_query($conn,$sql);
-          $resultCheck = mysqli_num_rows($result);
-
-          if ($resultCheck > 0) {
-            header("Location: ../signup.php?signup=usertaken");
-            exit();
-          }else {
-            //Hashing the password
-            $hashedPwd = password_hash($pwd,PASSWORD_DEFAULT);
-            //Insert the user into the database
-            $sql = "INSERT INTO users(user_first,user_last,user_email,user_uid,user_pwd) VALUES('$first','$last','$email','$uid','$hashedPwd');";
-            $result = mysqli_query($conn,$sql);
-            header("Location: ../signup.php?signup=success");
-            exit();
-          }
-        }
-      }
+}else{
+  //check if email is valid
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    header("Location: ../signup.php?signup=email");
+    exit();
+  }else{
+    $sql = "SELECT * FROM users WHERE user_uid='$uid'";
+    $result = mysqli_query($conn,$sql);
+    $resultCheck = mysqli_num_rows($result);
+    if($resultCheck > 0){
+      header("Location: ../signup.php?signup=usertaken");
+      exit();
+    }else{
+      //Hashing the password
+      $hashedPwd = password_hash($pwd,PASSWORD_DEFAULT);
+      //INSERT THE USAER INTO THE DATABASE
+      $sql = "INSERT INTO users (user_first, user_last,user_email,user_uid,user_pwd) VALUES ('$first','$last','$email','$uid','$hashedPwd');";
+      mysqli_query($conn,$sql);
+      header("Location: ../signup.php?signup=success");
+      exit();
     }
+   }
+  }
+}
 
-
-
-} else {
-   header("Location:../signup.php");
+}else{
+  header("Location: ../signup.php");
   exit();
 }
