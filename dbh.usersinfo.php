ff8a1569283bbe92a654f66442a789e0a8770beb
Ishantha Udara <ishanthaudara96@gmail.com>
2018-01-16 23:18:51 +0530

    fdsfaff

---
+++ dbh.usersinfo.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+
+
+?>
